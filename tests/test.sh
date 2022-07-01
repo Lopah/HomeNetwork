1972bf2ce6efec82ca3a9dc049859538de325437
Daniel Jašek <jasekdan@gmail.com>
2022-07-01 17:57:18 +0200

    wip tests

--- tests/test.sh
+++ tests/test.sh
@@ -4,7 +4,7 @@
 #
 # Usage: [OPTIONS] .tests/test.sh
 #   - distro: a supported Docker distro version (default = "ubuntu2004")
-#   - playbook: a playbook in the tests directory (default = "run.yml")
+#   - playbook: a playbook in the tests directory (default = "run.yaml")
 #   - cleanup: whether to remove the Docker container (default = true)
 #   - container_id: the --name to set for the container (default = timestamp)
 #   - test_idempotence: whether to test playbook's idempotence (default = true)
@@ -32,16 +32,32 @@ test_idempotence=${test_idempotence:-"true"}
 init="/lib/systemd/systemd"
 opts="--privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
 directory="$PWD/src"
+create_mac_vlan=${create_mac_vlan:-"true"}
+# reserved_ip_addresses=${reserved_ip_addresses:-("r1 192.168.0.2" "r2 192.168.0.3" "r3 192.168.0.4" "r4 192.168.0.5")}
+
+# Create MacVLAN network that bridges our container to the rest of the local network.
+if [ "$create_mac_vlan" = true ]; then
+  printf ${green}"Creating MacVLAN for the container to work locally.\n"
+  # excluded_addresses = ""
+  # for excluded_address in $reserved_ip_addresses
+  # do
+  # excluded_addresses += excluded_address
+  # done
+  docker network create -d macvlan \
+  --subnet=192.168.0.0/24 \
+  --gateway=192.168.0.1 \
+  -o parent=eth0.50 pub_net
+fi
 
 # Run the container using the supplied OS.
 printf ${green}"Starting Docker container: $docker_owner/docker-$distro-ansible."${neutral}"\n"
 docker pull $docker_owner/docker-$distro-ansible:latest
-docker run --detach --volume=$directory:/etc/ansible/playbooks/playbook_under_test:rw --env ANSIBLE_VAULT_PASSWORD_FILE=/etc/ansible/playbooks/playbook_under_test/passwordfile --name $container_id $opts $docker_owner/docker-$distro-ansible:latest $init
+docker run --detach --network=pub_net --dns="192.168.0.20" --volume=$directory:/etc/ansible/playbooks/playbook_under_test:rw --env ANSIBLE_VAULT_PASSWORD_FILE=/etc/ansible/playbooks/playbook_under_test/passwordfile --name $container_id $opts $docker_owner/docker-$distro-ansible:latest $init
 
 printf "\n"
 
-# Install requirements if `requirements.yml` is present.
-if [ -f "$directory/requirements.yml" ]; then
+# Install requirements if `requirements.yaml` is present.
+if [ -f "$directory/requirements.yaml" ]; then
   printf ${green}"Requirements file detected; installing dependencies."${neutral}"\n"
   docker exec --tty $container_id env TERM=xterm ansible-galaxy install -r /etc/ansible/playbooks/playbook_under_test/requirements.yaml
 fi
